df40eae58b7a7b28aaa5be4094933a97afbfd074
Paul Jonas <Paul.Jonas@dohle-net.de>
2024-07-19 14:00:11 +0200

    Added Condiions file
    Contains the Syntax of
    - if
    - else
    - elif

---
+++ 2_control_structures/1_conditions.py
@@ -0,0 +1,22 @@
+jana_alter = 12
+
+if jana_alter == 12:
+    print("Jana beucht die 6. Klasse.")
+
+
+hanna_alter = 13
+
+if hanna_alter == 12:
+    print("Hanna beucht die 6. Klasse.")
+else:
+    print("Hanna besucht nicht die 6. Klasse.")
+
+
+max_alter = 14
+
+if max_alter == 12:
+    print("Max beucht die 6. Klasse.")
+elif max_alter == 14:
+    print("Max beucht die 8. Klasse.")
+else:
+    print("Max besucht nicht die 6. Klasse und 8. Klasse.")
