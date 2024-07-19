1cda644677adb06319e4546e6e315ae7634c74d9
Paul Jonas <Paul.Jonas@dohle-net.de>
2024-07-19 11:30:06 +0200

    Added operators file
    operators contains
    -mathematical-
    - comparison-
    - logical oerators

---
+++ 3_operators.py
@@ -0,0 +1,36 @@
+# Arithmetische Operatoren
+print(3 + 4)  # 7
+
+print(3 - 4)  # -1
+
+print(3 * 4)  # 12
+
+print(3 / 4)  # 0.75
+
+print(3 % 4)  # 3
+
+print(3 ** 4)  # 81
+
+print(3 // 4)  # 0
+
+
+# Vergleichsoperatoren
+print(1 == 1)  # True
+
+print(1 != 1)  # False
+
+print(1 > 2)  # False
+
+print(1 < 2)  # True
+
+print(1 >= 2)  # False
+
+print(1 <= 2)  # True
+
+
+# Logische Operatoren
+print(True and True)  # True
+
+print(True or True)  # True
+
+print(not True)  # False
