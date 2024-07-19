0f8c3fb4382b9c8b8cbb5900f2ecce1bbb625704
Paul Jonas <Paul.Jonas@dohle-net.de>
2024-07-19 11:47:57 +0200

    Update 3_operators.py
    Added assignment operataors
    Seberated different oparators in console

--- 3_operators.py
+++ 3_operators.py
@@ -1,3 +1,10 @@
+# Imports nicht releavant für den Kurs
+from colorama import Fore, Style
+
+
+print(Fore.RED + "Arithmetische Operatoren" + Style.RESET_ALL)
+
+
 # Arithmetische Operatoren
 print(3 + 4)  # 7
 
@@ -12,6 +19,9 @@ print(3 % 4)  # 3
 print(3 ** 4)  # 81
 
 print(3 // 4)  # 0
+
+print()
+print(Fore.RED + "Vergleichsoperatoren" + Style.RESET_ALL)
 
 
 # Vergleichsoperatoren
@@ -28,9 +38,42 @@ print(1 >= 2)  # False
 print(1 <= 2)  # True
 
 
+print()
+print(Fore.RED + "Logische Operatoren" + Style.RESET_ALL)
+
+
 # Logische Operatoren
 print(True and True)  # True
 
 print(True or True)  # True
 
 print(not True)  # False
+
+
+print()
+print(Fore.RED + "Zuweisungsoperatoren" + Style.RESET_ALL)
+
+
+# Zuweisungsoperatoren
+a = 2
+
+a += 3  # a = a + 3
+print(a)
+
+a -= 3  # a = a - 3
+print(a)
+
+a *= 3  # a = a * 3
+print(a)
+
+a /= 3  # a = a / 3
+print(a)
+
+a %= 3  # a = a % 3
+print(a)
+
+a **= 3  # a = a ** 3
+print(a)
+
+a //= 3  # a = a // 3
+print(a)
