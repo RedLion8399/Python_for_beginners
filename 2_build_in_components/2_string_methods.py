fb978c01dc7c0b9c3f106ab8a83b4e70d6113a64
Paul Jonas <Paul.Jonas@dohle-net.de>
2024-07-21 00:35:14 +0200

    Added string_methods
    Contains most important build in methods for strings

---
+++ 2_build_in_components/2_string_methods.py
@@ -0,0 +1,46 @@
+# capitalize
+# Konvertiert den ersten Buchstaben einer Zeichenkette in Großbuchstaben
+text = "hello, world!"
+print(text.capitalize())
+
+
+# upper
+# Konvertiert alle Buchstaben in Großbuchstaben
+text = "Hello, world!"
+print(text.upper())
+
+
+# lower
+# Konvertiert alle Buchstaben in Kleinbuchstaben
+text = "Hello, world!"
+print(text.lower())
+
+
+# swapcase
+# Konvertiert Groß- und Kleinbuchstaben
+text = "Hello, world!"
+print(text.swapcase())
+
+
+# title
+# Konvertiert den ersten Buchstaben jeder Woerter in Großbuchstaben
+text = "hello, world!"
+print(text.title())
+
+
+# replace
+# Ersetzt substring in einer Zeichenkette
+text = "hello, world!"
+print(text.replace("hello", "bye"))
+
+
+# join
+# Verbindet Elemente einer Liste durch eim Füllzeichen
+text = ["hello", "world!"]
+print(" ".join(text))
+
+
+# split
+# Auftrennen eines Strings in Elemente
+text = "hello world what a beautiful day"
+print(text.split(", ")) 
